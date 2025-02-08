d8de96b64e7afee4d286e9ae16b835423c03deaf
Lucas Martins Vasconcelos <lmartins2205@gmail.com>
2025-02-08 15:34:56 -0300

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/.vite/'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@mui|react-router-dom)/)",
+  ],
+};
+
+export default config;
